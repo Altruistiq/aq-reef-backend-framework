de81caf2f5831828905c9ac8bb1116d3956b6511
George Rempousis <george@altruistiq.com>
2023-12-09 15:11:55 +0200

    fix: fixed test script

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -3,9 +3,9 @@ set -e
 currentDir=$(pwd)
 rm -rf "$currentDir/tests/test-server/node_modules"
 rm -rf "$currentDir/tests/test-server/reef"
-pnpm --prefix "$currentDir/tests/test-server" install
+npm --prefix "$currentDir/tests/test-server" install
 rm -rf ./pkg
-pnpm tsc
+npm tsc
 ln -s "$currentDir/pkg" "$currentDir/tests/test-server/reef"
 if [ "$1" = "watch" ]; then
     npx mocha --config .mocharc.json --watch
