72c85c656478b6e7561b96dd355f698e8872452b
gremp <me@gremp.gr>
2024-04-24 15:17:52 +0300

    fix: fixed traceid path

--- src/helpers/trace-id.helper.ts
+++ src/helpers/trace-id.helper.ts
@@ -1,7 +1,7 @@
 import type { Response } from "express";
 
 export function getTraceId(res?: Response): string | undefined {
-	if (res?.locals?.traceId) return res.locals.traceId;
+	if (res?.locals?.reef?.traceId) return res.locals.reef.traceId;
 	const err = new Error();
 	const traceIdSearch = /\[as __REEF_CALL_STACK_(.+?)__END_OF_REEF__]/gm.exec(
 		err.stack as string,
