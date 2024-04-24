d9747b4b218d153c7c6d55cd5005980ae78b1ee2
gremp <me@gremp.gr>
2024-04-24 15:15:54 +0300

    feat: faster way to get traceId

--- src/helpers/trace-id.helper.ts
+++ src/helpers/trace-id.helper.ts
@@ -1,4 +1,7 @@
-export function getTraceId(): string | undefined {
+import type { Response } from "express";
+
+export function getTraceId(res?: Response): string | undefined {
+	if (res?.locals?.traceId) return res.locals.traceId;
 	const err = new Error();
 	const traceIdSearch = /\[as __REEF_CALL_STACK_(.+?)__END_OF_REEF__]/gm.exec(
 		err.stack as string,
