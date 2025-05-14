99af0b09627464c6da73b3c29cb5b1695d6eba90
Michael Turner <113600162+mt-aq@users.noreply.github.com>
2025-05-14 12:38:29 +0100

    Update src/decorators/index.ts
    Co-authored-by: George Rempousis <me@gremp.gr>

--- src/decorators/index.ts
+++ src/decorators/index.ts
@@ -22,4 +22,13 @@ export {
 	Next,
 } from './base-param-decorators.class';
 export { Log, Logger } from './log.decorator';
-export {} from './symbols';
+export {
+  endpointMetaSymbol,
+  paramMetaSymbol,
+  controllerMetaSymbol,
+  loggerMetadataKeySymbol,
+  middlewareControllerKey,
+  directMiddlewareSymbol,
+  preExecutionHookSymbol,
+  afterResponseExecutionHookSymbol,
+} from './symbols';
